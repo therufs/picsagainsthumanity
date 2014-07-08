515b974c3ab0ef7cff68b12ba8df1a5fd56b42c2
Ruthan <rcfreese@gmail.com>
2014-07-08 15:57:55 -0400

    Pics uploadable & visible

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -12,7 +12,7 @@ class AnswersController < ApplicationController
     # @answer.question_id = current_question.id
     @answer.user_id = current_user.id ## not sure if this is good/necessary
     if @answer.save
-      redirect_to answer_path[answer], notice: "Answer submitted!"
+      redirect_to answer_path(@answer.id), notice: "Answer submitted!"
     else
       render 'new'
     end

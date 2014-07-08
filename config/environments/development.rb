12389da6a3c55d48925d7a3cbb8636af99e7a770
Ruthan <rcfreese@gmail.com>
2014-07-07 22:31:13 -0400

    Add confirmation mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,6 +5,10 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+
+
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
   config.cache_classes = false

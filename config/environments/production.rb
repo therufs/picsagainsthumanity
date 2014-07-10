3bd8bda39f06b4cc365f400fdc526fb0e74daf98
Ruthan <rcfreese@gmail.com>
2014-07-09 21:29:13 -0400

    Update config for heroku some more

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,27 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-Mail.defaults do
-  delivery_method :smtp, {
-    :address => ENV['POSTMARK_SMTP_SERVER'],
-    :port => '25',
-    :domain => 'yourapp.heroku.com',
-    :user_name => ENV['POSTMARK_API_KEY'],
-    :password => ENV['POSTMARK_API_KEY'],
-    :authentication => :plain,
-    :enable_starttls_auto => true
-  }
-end
-
-ActionMailer::Base.smtp_settings = {
-  :port           => '25',
-  :address        => ENV['POSTMARK_SMTP_SERVER'],
-  :user_name      => ENV['POSTMARK_API_KEY'],
-  :password       => ENV['POSTMARK_API_KEY'],
-  :domain         => 'yourapp.heroku.com',
-  :authentication => :plain,
-}
-ActionMailer::Base.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'boiling-mountain-7261.herokuapp.com' }
+  config.action_mailer.delivery_method = :postmark
+  config.action_mailer.postmark_settings = { :api_key => ENV['POSTMARK_API_KEY'] }
 
 # config.action_mailer.delivery_method = :postmark
 # config.action_mailer.postmark_settings = { api_key: ENV['POSTMARK_API_KEY']}
